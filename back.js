c005f5430c0d4c4e8882cdb05cdf62921547bee2
grimsi1ence <144237328+grimsi1ence@users.noreply.github.com>
2023-10-01 18:38:51 +0300

    Update back.js

--- back.js
+++ back.js
@@ -1,1 +1,11 @@
 //  ¯\_(ツ)_/¯
+const http = require('http');
+
+const server = http.createServer((request, response) => {
+  response.write('Hello, world!');
+  response.end();
+});
+
+server.listen(1000, () => {
+  console.log('Server is listening on port 1000');
+});
